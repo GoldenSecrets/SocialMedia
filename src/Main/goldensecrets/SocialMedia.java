ebcaa925a85368f7dfe2efb2f8905098291fb6c4
GoldenSecrets <raerae734@gmail.com>
2020-08-13 03:06:02 -0400

    Initial Commit

---
+++ src/Main/goldensecrets/SocialMedia.java
@@ -0,0 +1,8 @@
+package me.goldensecrets;
+
+import kr.entree.spigradle.annotations.PluginMain;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@PluginMain
+public class SocialMedia extends JavaPlugin {
+}
