5bac8ee9a2d750fc9e1eb8e92ac05c511c883ae9
Richard Simpson <magnum1997@gmail.com>
2020-08-13 19:16:02 -0700

    Add imports

--- src/main/me/goldensecrets/SocialMedia.java
+++ src/main/me/goldensecrets/SocialMedia.java
@@ -1,23 +1,26 @@
 package me.goldensecrets;
 
+import kr.entree.spigradle.annotations.PluginMain;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class SocialMedia extends Java {
+@PluginMain
+public class SocialMedia extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        //Register our command "Discord"
-        this.getCommand("discord").setExecutor(new CommandDiscord());
-    }
+	@Override
+	public void onEnable () {
+		//Register our command "Discord"
+		this.getCommand("discord").setExecutor(new CommandDiscord());
+	}
 
-    @Override
-    public void onEnable() {
-        //Register our command "Instagram"
-        this.getCommand("instagram").setExecutor(new CommandInstagram());
-    }
+	@Override
+	public void onEnable () {
+		//Register our command "Instagram"
+		this.getCommand("instagram").setExecutor(new CommandInstagram());
+	}
 
-    @Override
-    public void onEnable() {
-        //Register our command "YouTube"
-        this.getCommand("youtube").setExecutor(new CommandYouTube());
-    }
+	@Override
+	public void onEnable () {
+		//Register our command "YouTube"
+		this.getCommand("youtube").setExecutor(new CommandYouTube());
+	}
 }
